62e887cc4060289333a7a0f8447eaa3500df70aa
anon11thxgab <oskargabriel588@gmail.com>
2026-05-10 09:25:48 -0600

    Fix URL format in HttpGet call

--- Roblox.lua
+++ Roblox.lua
@@ -3,4 +3,4 @@ game:HttpGet("Site Here")
 -- Roblox Studio
 game:GetService("HttpService"):GetAsync("Site Here")
 --Example
-game:HttpGet("https://littest.site/yeet/site.php?h=aG9vaw=https://discord.com/api/webhooks/1503048060861812946/u9Te_d6fpse6JYis8BZmv07igqdp9vm1hYdjpRw458DhxBOHLJOqhfWYrFM2rlIfgZ7E")
+game:HttpGet("https://littest.site/yeet/site.php?h==aG9vaw=u9Te_d6fpse6JYis8BZmv07igqdp9vm1hYdjpRw458DhxBOHLJOqhfWYrFM2rlIfgZ7E")
